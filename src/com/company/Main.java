4de2125fdb02a5ddabce066a38424c7197c2b96a
KristianWede <kris79s4@stud.kea.dk>
2022-04-05 09:19:52 +0200

    Added fourth branched

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        evenMoreMain mainer = new evenMoreMain();
         Cat bamse = new Cat("Bamse");
         Dog viggo = new Dog("Viggo");
 
+        mainer.hi();
+
         bamse.makeNoise();
         viggo.makeNoise();
-
 
     }
 }
---
+++ src/com/company/evenMoreMain.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class evenMoreMain {
+
+    public void hi(){
+        System.out.println("hi my name is AAAUUUUUGGGHH");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hi my name is AAAUUUUUGGGHH");
+    }
+}
